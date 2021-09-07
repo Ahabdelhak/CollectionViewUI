0962e77c7e16df2b85c142c9dd68069faf826fe7
ah_abdelhak <ah_abdelhak@yahoo.com>
2021-09-07 15:29:38 +0200

    collection view sample demo

---
+++ CollectionViewDemo/MyCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MyCollectionViewCell.swift
+//  CollectionViewDemo
+//
+//  Created by Ahmed Sabry on 07/09/2021.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var labelUI: UILabel!
+    
+}
